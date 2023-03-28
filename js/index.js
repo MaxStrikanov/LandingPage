c70f347e0f759feb460da66c7a93c12a84a0ccdc
Max Strikanov <maxvell.88@mail.ru>
2023-03-28 14:01:12 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,52 +1,13 @@
-const menuToggle = document.querySelector('.menu-toggle input ');
-const menuItems = document.querySelector('.menu-items');
-
-menuToggle.addEventListener('click', () => {
-  menuItems.classList.toggle('open');
-});
+import checkingСertificate  from '../components/xlsx.js';
+import toggleMenu from '../components/menu.js';
+import slider from '../components/slider.js'
 
 
-var sliderSelector = '.swiper-container',
-options = {
-    init: false,
-    loop: true,
-    speed: 400,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    centeredSlides : true,
-    effect: 'coverflow',
-            coverflowEffect: {
-                //угол
-            rotate: 4,
-            //тень
-            slideShadows: false,
-            //наложение
-            stretch: 0,
-            modifier: 2,
-            //удаление
-            depth: 400,  
-			scale: 1,
-    },
-    grabCursor: true,
-    parallax: false,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1000: {
-            slidesPerView: 2,
-            spaceBetween: 0
-        },
-        767: {
-            slidesPerView: 2,
-            spaceBetween: -20
-        }        
-        }
-};
-var mySwiper = new Swiper(sliderSelector, options);
-mySwiper.init();
+document.addEventListener('load' , checkingСertificate())
+toggleMenu();
+slider();
+
+
+
+
+
